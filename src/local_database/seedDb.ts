53baed3157bb831869af290dd51a143c8c3bdc81
Angry Coding <mailidanielifechukwu@gmail.com>
2024-06-10 22:44:13 +0100

    Seed Db

---
+++ src/local_database/seedDb.ts
@@ -0,0 +1,22 @@
+import fs from "fs";
+import { db } from "@/services/db";
+//import { createUser, User } from "@/services/db/user";
+import "@/services/firebase";
+
+
+export async function seedDatabase() {
+  // const existingUsers = await db.users.all();
+  // if (existingUsers.length > 0) {
+  //   console.log("*** Local firebase is not empty. Skipping seed import...");
+  //   return;
+  // }
+
+  // const USER_SEED_DATA = "./local_database/users.json";
+  // const seedUsers = JSON.parse(fs.readFileSync(USER_SEED_DATA, "utf8")) as User[];
+  
+  //  seedUsers.forEach(async (userData)=> {
+  //       const { username,id, first,last, lang, referedBy } = userData as User;
+  //        await createUser(id,referedBy,username,first,last,lang);
+  //  })
+
+}
