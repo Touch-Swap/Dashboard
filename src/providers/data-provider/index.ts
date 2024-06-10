420d944d0801bb693573cf689bd4bd1c6457cbd6
Angry Coding <mailidanielifechukwu@gmail.com>
2024-06-10 22:45:03 +0100

    Db Changes

--- src/providers/data-provider/index.ts
+++ src/providers/data-provider/index.ts
@@ -2,6 +2,6 @@
 
 import dataProviderSimpleRest from "@refinedev/simple-rest";
 
-const API_URL = "http://localhost:4000";
+const API_URL = "http://localhost:3000/api/";
 
 export const dataProvider = dataProviderSimpleRest(API_URL);
