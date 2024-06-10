cb28983eec32cd36fe04916adc2002ec227af424
Angry Coding <mailidanielifechukwu@gmail.com>
2024-06-10 22:44:24 +0100

    Db File

---
+++ src/services/db/index.ts
@@ -0,0 +1,30 @@
+import { Task } from "./task";
+import { User } from "./user";
+import { Typesaurus, schema } from "typesaurus";
+
+// Generate the db object from given schem that you can use to access
+// Firestore, i.e.:
+//   await db.get(userId)
+export const db = schema($ => ({
+      users: $.collection<User>(),
+      tasks: $.collection<Task>()
+}));
+
+
+export type Schema = Typesaurus.Schema<typeof db>;
+
+export type SchemaKeys = keyof Schema;
+export type SubSchemas = Schema[SchemaKeys]["sub"];
+export type SubSchemaKeys = keyof SubSchemas;
+export type Document = Schema[SchemaKeys]["Doc"];
+export type SubDocument = SubSchemas[SubSchemaKeys]["Doc"];
+
+export type Result<T> = {
+  id: string;
+} & T & {
+    exist: boolean;
+  };
+
+export function toResult<U>(doc: Document | SubDocument | null): Result<U> {
+  return { id: doc?.ref?.id as string, ...(doc?.data as U), exist: !!doc };
+}
