91fe177dcaf43380226cc4166dcad41011d9b90d
Angry Coding <mailidanielifechukwu@gmail.com>
2024-06-11 02:28:13 +0100

    changes

--- src/util/index.ts
+++ src/util/index.ts
@@ -1,6 +1,4 @@
 export function paginate(items:any[], start = 1, end = 10) {
-    console.log(items)
     const paginatedItems = items.slice(start, end);
-    console.log(paginatedItems)
     return paginatedItems
 }
